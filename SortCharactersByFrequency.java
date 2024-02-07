2e3fed74d23a198daadeaac09860c47a10d0ffc6
saurabhsingh <saurabh623a@gmail.com>
2024-02-08 00:17:50 +0530

    Day-1

--- SortCharactersByFrequency.java
+++ SortCharactersByFrequency.java
@@ -20,7 +20,28 @@ public class SortCharactersByFrequency {
             return result.toString();
         }
     }
+    static class WithoutUsingMap {
+            public static String frequencySort(String s) {
+                char[] str=s.toCharArray();
+                int[] freq=new int[128];
+                // for(char ch:s) freq[ch]++;
+                for (char c : str) freq[c]++;
+
+                for(int i=0;i<str.length;){
+                    char cmax=',';
+                    for(int j=0;j<128;j++){
+                        if(freq[j]>freq[cmax]) cmax=(char)j;
+                    }
+                    while(freq[cmax]!=0){
+                        str[i++]=cmax;
+                        freq[cmax]--;
+                    }
+                }
+                return new String(str);
+            }
+        }
     public static void main(String[] args) {
         System.out.println(UsingHashmap.frequencySort("tree"));
+        System.out.println(WithoutUsingMap.frequencySort("tree"));
     }
 }
