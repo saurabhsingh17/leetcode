dbec4f301db03640dd25091aff46803533e024ed
saurabhsingh <saurabh623a@gmail.com>
2024-02-14 21:45:25 +0530

    Day-4

--- Leetcode39.java
+++ Leetcode39.java
@@ -7,7 +7,13 @@ public class Leetcode39 {
 
         int[] c = {10, 1, 2, 7, 6, 1, 5};
         int target = 8;
-        System.out.println(combinationSumm(c, target));
+        System.out.println(combinationSum(c, target));
+    }
+    static List<List<Integer>> combinationSum(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        List<List<Integer>> result = new ArrayList<>();
+        solve2(candidates,new ArrayList<>(),0,target,result);
+        return result;
     }
 
     static List<List<Integer>> combinationSumm(int[] candidates, int target) {
@@ -29,5 +35,18 @@ public class Leetcode39 {
         solve(candidates, new ArrayList<>(output), target - candidates[index], index + 1, result);
         output.remove(output.size() - 1);
         solve(candidates, new ArrayList<>(output), target, index + 1, result);
+    }
+    static void solve2(int[] candidates,List<Integer> output, int pos, int target,List<List<Integer>> result){
+        if (target == 0) {
+            result.add(output);
+        }
+        if(target <= 0) {
+            return;
+        }
+        for (int i = pos; i < candidates.length ; i++) {
+            output.add(candidates[i]);
+            solve2(candidates,new ArrayList<>(output),i,target-candidates[i],result);
+            output.remove(output.size()-1);
+        }
     }
 }
